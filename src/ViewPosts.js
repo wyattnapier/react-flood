6a1b20db5b3c25fc024b1064b6b04a78a02957bb
Wyatt <wnapier@bu.edu>
2023-07-14 16:11:15 -0400

    working towards using country-state-city for dropdowns

--- src/ViewPosts.js
+++ src/ViewPosts.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { people } from './data.js';
+import { Country, State, City } from 'country-state-city';
+import { getStatesOfCountry } from 'country-state-city/lib/state.js';
 
-export default function viewPosts () {
+export default function ViewPosts () {
     const vtpeeps = people.filter((person) =>
         person.state === 'Vermont'
     );
@@ -11,20 +13,38 @@ export default function viewPosts () {
     //state selection
     //is there a way that I can use a library to grab all towns from each state?
     //also can I autogenerate a list of states instead?
-    //const [selectedState, setSelectedState] = useState('');
-    // function handleStateChange (e) {
-    //     setSelectedState(e.target.value);
-    // }
+    const [selectedState, setSelectedState] = useState('');
+    function handleStateChange (e) {
+        setSelectedState(e.target.value);
+    }
 
     //is there a way to use a library to autogenerate main towns from states?
-    //const [town, setTown] = useState('Town name');
-    // function handleTownChange(e) {
-    //     setTown(e.target.value);
-    // }
+    const [town, setTown] = useState('Town name');
+    function handleTownChange(e) {
+        setTown(e.target.value);
+    }
+
+    const stateOptions = getStatesOfCountry('US').map(state => ({
+        value: state.stateCode,
+        label: state.name
+    }));
 
     return (
         <div>
             <p>AHEM! I'm trying to view a post here :/</p>
+            <h1>Let's filter this out:</h1>
+            <h3>State:</h3>
+            <select value={selectedState} onChange={handleStateChange}>
+                <option value=""> -- Select --</option>
+                <option value="VT">Vermont</option>
+                <option value="NH">New Hampshire</option>
+            </select>
+            <h3>Town:</h3>
+            <textarea 
+                placeholder='town'
+                value={town}
+                onChange={handleTownChange}
+            />
             <p>Everyone:</p>
             <ul>
                 {people.map(person =>

bc4a0ea1a55370c6eab975896788076022571577
Wyatt <wnapier@bu.edu>
2023-07-13 22:47:04 -0400

    added 'state' criteria to test data

--- src/data.js
+++ src/data.js
@@ -2,20 +2,25 @@ export const people = [{
     id: 0,
     name: 'Creola Katherine Johnson',
     profession: 'mathematician',
+    state: 'Vermont',
 }, {
     id: 1,
     name: 'Mario José Molina-Pasquel Henríquez',
     profession: 'chemist',
+    state: 'Vermont',
 }, {
     id: 2,
     name: 'Mohammad Abdus Salam',
     profession: 'physicist',
+    state: 'New Hampshire',
 }, {
     id: 3,
     name: 'Percy Lavon Julian',
     profession: 'chemist',  
+    state: 'New Hampshire'
 }, {
     id: 4,
     name: 'Subrahmanyan Chandrasekhar',
     profession: 'astrophysicist',
+    state: 'Vermont'
 }];

1377db981840104dc34ba0544d7e1fcb0a741aa4
Wyatt <wnapier@bu.edu>
2023-07-13 22:23:52 -0400

    added filtering for displayed data by state

--- src/ViewPosts.js
+++ src/ViewPosts.js
@@ -2,13 +2,27 @@ import Post from './Post.js'
 import { people } from './data.js';
 
 export default function viewPosts () {
-    // component for each post that 
+    const vtpeeps = people.filter(person =>
+        person.state === 'Vermont'
+    );
     return (
         <div>
             <p>AHEM! I'm trying to view a post here :/</p>
             <Post />
+            <p>Everyone:</p>
             <ul>
                 {people.map(person =>
+                    <li key={person.id}>
+                        <p>
+                            <b>{person.name}:</b>
+                            {' is a ' + person.profession + '.'}
+                        </p>
+                    </li>
+                )}
+            </ul>
+            <p>Now just the Vermonters!</p>
+            <ul>
+                {vtpeeps.map(person =>
                     <li key={person.id}>
                         <p>
                             <b>{person.name}:</b>
